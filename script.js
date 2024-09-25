f99ed766aa3253c0e5bbbe15d148ea2af0ba3a58
Rjack00 <rjackson77@comcast.net>
2024-09-24 19:07:02 -0600

    script.js updated
    unsaved changes saved for previous commit

--- script.js
+++ script.js
@@ -71,6 +71,8 @@ class Platform {
   }
 }
 
+
+
 const player = new Player();
 
 const platformPositions = [
@@ -132,9 +134,20 @@ const animate = () => {
     if (collisionDetectionRules.every((rule) => rule)) {
       player.velocity.y = 0;
       return;
-
-
     }
+    
+    const platformDetectionRules = [
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+      player.position.y + player.height >= platform.position.y,
+      player.position.y <= platform.position.y + platform.height,
+    ];
+
+    if (platformDetectionRules.every(rule => rule)) {
+      player.position.y = platform.position.y + player.height;
+      player.velocity.y = gravity;
+    };
   });
 }
 
